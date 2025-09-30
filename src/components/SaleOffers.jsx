6320d13d33193132229d73b6d75a46db1a15a385
Tim Waring <waring@adobe.com>
2025-09-30 10:11:53 +0100

    sss

--- src/components/SaleOffers.jsx
+++ src/components/SaleOffers.jsx
@@ -61,65 +61,7 @@ const SaleOffers = ({ offers, setFetchTrigger }) => {
         </div>
 
   
-          <div className="so-tiles">
-
-            <div className="so-tile">
-              <div className="so-tile-wrapper">
-                <div className="so-img-content">
-                  <img src={SO1} alt="British Airways" className="so-img" />
-                </div>
-                <div className="so-text-content">
-                  <div className="so-title">Miami flights</div>
-                  <div className="so-from">From</div>
-                  <div className="so-price">£434</div>
-                  <div className="so-text">Return from London, Mar 2025</div>
-                </div>
-              </div>
-            </div>
-
-            <div className="so-tile">
-              <div className="so-tile-wrapper">
-                <div className="so-img-content">
-                  <img src={SO2} alt="British Airways" className="so-img" />
-                </div>
-                <div className="so-text-content">
-                  <div className="so-title">Flights to St Lucia</div>
-                  <div className="so-from">From</div>
-                  <div className="so-price">£441</div>
-                  <div className="so-text">Return from London, Mar 2025</div>
-                </div>
-              </div>
-            </div>
-
-            <div className="so-tile">
-              <div className="so-tile-wrapper">
-                <div className="so-img-content">
-                  <img src={SO3} alt="British Airways" className="so-img" />
-                </div>
-                <div className="so-text-content">
-                  <div className="so-title">Barbados. flights + 7 nights</div>
-                  <div className="so-from">From</div>
-                  <div className="so-price">£739</div>
-                  <div className="so-text">Per person from London, Sep 2025</div>
-                </div>
-              </div>
-            </div>
-
-            <div className="so-tile">
-              <div className="so-tile-wrapper">
-                <div className="so-img-content">
-                  <img src={SO4} alt="British Airways" className="so-img" />
-                </div>
-                <div className="so-text-content">
-                  <div className="so-title">New York flights + 3 nights</div>
-                  <div className="so-from">From</div>
-                  <div className="so-price">£599</div>
-                  <div className="so-text">Per person from London, Feb 2025</div>
-                </div>
-              </div>
-            </div>
-
-          </div>
+ 
 
       </div>
     </div>
